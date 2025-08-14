8b36cb49d86fddcf56ad6e56b6bba62afd4a8a37
Iverson <sarmientoiverson12@gmail.com>
2025-08-14 14:53:17 +0800

    responsiveness

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/W-Chat',
+  base: '/W-Chat/',
   build: {
     outDir: 'docs'
   }
